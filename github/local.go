7772440ab1496e73d8ecd7061e61157a718355f9
Felipe Soler <soler.felipe@gmail.com>
2026-04-11 01:42:16 +0200

    use git from path

--- github/local.go
+++ github/local.go
@@ -12,17 +12,22 @@ import (
 func Clone(repository *Repository, destination string) error {
 	ctx, cancelFunc := context.WithTimeout(context.Background(), 1*time.Minute)
 	defer cancelFunc()
-	return run(ctx, "git", "clone", repository.CloneUrl, destination)
+	return run(ctx, "clone", repository.CloneUrl, destination)
 }
 
-func run(ctx context.Context, name string, args ...string) error {
+func run(ctx context.Context, args ...string) error {
+	git, err := exec.LookPath("git")
+	if err != nil {
+		return fmt.Errorf("git not found: %w", err)
+	}
+
 	var stderr bytes.Buffer
-	cmd := exec.CommandContext(ctx, fmt.Sprintf(`/usr/bin/%s`, name), args...)
+	cmd := exec.CommandContext(ctx, git, args...)
 	cmd.Stdout = nil
 	cmd.Stderr = &stderr
 
-	if err := cmd.Run(); err != nil {
-		command := strings.Join(append([]string{name}, args...), " ")
+	if err = cmd.Run(); err != nil {
+		command := strings.Join(append([]string{git}, args...), " ")
 		return fmt.Errorf("%v: %w %v", command, err, stderr.String())
 	}
 	return nil
